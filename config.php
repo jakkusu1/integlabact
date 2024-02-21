4764ea6ede45b938369b574a8a5c30aa2f63ada2
jakkusu1 <olango.jabezjames@gmail.com>
2024-02-22 00:21:46 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = '';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    ?>
+
+    <form action="process.php" method="post">
+        <label for="firstName">First Name</label><br>
+        <input type="text" name="firstName"><br>
+
+        <label for="middleName">Middle Name</label><br>
+        <input type="text" name="middleName"><br>
+
+        <label for="lastName">Last Name</label><br>
+        <input type="text" name="lastName"><br>
+
+        <label for="username">Username</label><br>
+        <input type="text" name="username"><br>
+
+        <label for="password">Password</label><br>
+        <input type="text" name="password"><br>
+
+        <input type="submit" name="submitButton">
+    </form>
+</body>
+</html>
