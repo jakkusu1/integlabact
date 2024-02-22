1f911800fc08a0ada2c6ea4c0a42ae4486fc279c
jakkusu1 <olango.jabezjames@gmail.com>
2024-02-22 12:39:41 +0800

    completed initial functionality

--- config.php
+++ config.php
@@ -2,8 +2,12 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = '';
+$dbname = 'integlab';
 
 $conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
--- process.php
+++ process.php
@@ -2,6 +2,22 @@
 include("config.php");
 
 if(isset($_POST["submitButton"])){
-    
+    $firstName = $_POST["firstName"];
+    $middleName= $_POST["middleName"];
+    $lastName = $_POST["lastName"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    // $role = $_POST["role"];
+
+    $query = "INSERT INTO users (firstName, middleName, lastName, username, password) VALUES ('$firstName', '$middleName', '$lastName', '$username', '$password')";
+
+    if ($conn->query($query) === TRUE) {
+        echo "Registered Successfully";
+      } else {
+        echo "Registration Failed";
+        // echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+      
+    $conn->close();
 }
 ?>
